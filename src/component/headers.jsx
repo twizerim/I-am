70af0022dafefb286b55e27d9c5d8653de953a54
TwizerimanaGedeon <emmanueldusengimana83@gmail.com>
2023-12-18 15:12:17 +0200

    navbar

---
+++ src/component/headers.jsx
@@ -0,0 +1,21 @@
+
+
+
+function Header(){
+
+    return (
+        <nav>
+            <div className="log"></div>
+            <div className="links">
+                <ul className="link">
+                    <li className="nav-link">Welcome</li>
+                    <li className="nav-link">I'am</li>
+                    <li className="nav-link">Contact-me</li>
+                    <li className="nav-link">Blogs</li>
+                    <li className="nav-link">Login</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Header
